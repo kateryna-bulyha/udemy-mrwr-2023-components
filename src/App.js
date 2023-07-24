59c39408df25320e64d28120aac4cfe0809cd009
Kateryna Bulyha <katerinabulyha@gmail.com>
2023-07-24 12:43:55 +0200

    done foundation for the app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Button from "./Button";
+
+function App () {
+    return <div>
+        <div>
+            <Button>Click me!</Button>
+        </div>
+        <div>
+            <Button>Buy now!</Button>
+        </div>
+        <div>
+            <Button>See Deal!</Button>
+        </div>
+        <div>
+            <Button>Hide Ads!</Button>
+        </div>
+        <div>
+            <Button>Surprise!</Button>
+        </div> 
+    </div>
+}
+
+export default App;
---
+++ src/Button.js
@@ -0,0 +1,20 @@
+
+
+function Button ({ 
+    children,
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    outline,
+    rounded
+}) {
+    if (primary && secondary) {
+        throw new Error('Only one of primary and secondary should be provided')
+    }
+
+    return <button>{ children }</button>
+}
+
+export default Button;
